6d601f602ace510ea808be4cb408caf0ea6a56ef
Aleksandr <alcoope8@asu.edu>
2023-11-24 20:02:02 -0700

    Added Framework and functionality

--- app/src/main/java/com/example/asu_crimestats/SecondFragment.kt
+++ app/src/main/java/com/example/asu_crimestats/SecondFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.asu_crimestats.databinding.FragmentSecondBinding
 
 /**
@@ -19,22 +21,71 @@ class SecondFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    // Reference to the report_view Text
+    private lateinit var reportView     : TextView
+
+    // References to the edit text fields
+    private lateinit var street_text    : EditText
+    private lateinit var building_text  : EditText
+    private lateinit var time_text      : EditText
+
+    // References to the buttons
+    private lateinit var b_find_crimes_by_street    : Button
+    private lateinit var b_find_crimes_by_building  : Button
+    private lateinit var b_find_crimes_by_time      : Button
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
-
+    ): View {
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
+    // Do Stuff Here:
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        // Initialize report view reference
+        reportView = view.findViewById(R.id.report_view)
+
+        // Initialize edit text fields
+        street_text     = view.findViewById(R.id.edt_streetName)
+        building_text   = view.findViewById(R.id.edt_buildingName)
+        time_text       = view.findViewById(R.id.edt_time)
+
+        // Initialize buttons
+        b_find_crimes_by_street     = view.findViewById(R.id.b_find_crimes_by_street)
+        b_find_crimes_by_building   = view.findViewById(R.id.b_find_crimes_by_building)
+        b_find_crimes_by_time       = view.findViewById(R.id.b_find_crimes_by_time)
+
+        // Add actions to the buttons:
+        b_find_crimes_by_street.setOnClickListener{
+            // Get text from appropriate text field
+            val streetString = street_text.text.toString()
+
+            // When button is clicked, change the text view
+            updateReportTextView("Querying DB for reports on street: $streetString")
         }
+
+        b_find_crimes_by_building.setOnClickListener{
+            // Get text from appropriate text field
+            val buildingString = building_text.text.toString()
+
+            // When button is clicked, change the text view
+            updateReportTextView("Querying DB for reports on building: $buildingString")
+        }
+
+        b_find_crimes_by_time.setOnClickListener{
+            // Get text from appropriate text field
+            val timeString = time_text.text.toString()
+
+            // When button is clicked, change the text view
+            updateReportTextView("Querying DB for reports at time: $timeString")
+        }
+    }
+    private fun updateReportTextView(newText: String){
+        reportView.text = newText
     }
 
     override fun onDestroyView() {
